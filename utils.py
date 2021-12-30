a14bf0e05f773396530c02009f67f0143eb6e2c5
Adrien Burq <burq.adrien@gmail.com>
2021-12-30 18:58:26 +0100

    add plot function

--- utils.py
+++ utils.py
@@ -2,8 +2,10 @@
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
 import pandas as pd
+import geopandas as gpd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
 
 
 # %%
@@ -124,3 +126,47 @@ def clean_data_brouillon(df):
 # # %%
 # new_df.columns
 # # %%
+
+def get_liste_district_cluster(list_admLvl, df_admLvl_cluster, admLvl):
+    liste_admLvl_cluster = []
+    for i in range(len(list_admLvl)):
+        l = []
+        l.append(list_admLvl[i])
+        print(df_admLvl_cluster[df_admLvl_cluster[admLvl] == list_admLvl[i]]["cluster"].to_numpy())
+        if len(df_admLvl_cluster[df_admLvl_cluster[admLvl] == list_admLvl[i]]["cluster"].to_numpy().astype(int)) > 0 :
+            l.append(np.bincount(df_admLvl_cluster[df_admLvl_cluster[admLvl] == list_admLvl[i]]["cluster"].to_numpy().astype(int)).argmax())
+        liste_admLvl_cluster.append(l)
+    return liste_admLvl_cluster
+
+
+#plot clusters on map of India
+
+def plot_on_map(method_labels,pathData,admLvl):      # typiquement admLvl = 'District'
+    labels_df = pd.DataFrame(method_labels, columns=['labels'])
+    df_init = pd.read_csv(pathData)
+    df_init['cluster'] = labels_df['labels'] 
+    df_admLvl_cluster = df_init[['District', 'cluster']]
+
+    list_admLvl = pd.unique(df_admLvl_cluster[admLvl])
+
+    list_district_cluster = get_liste_district_cluster(list_admLvl, df_admLvl_cluster, admLvl)
+    df_reduced = pd.DataFrame(list_district_cluster, columns=[admLvl, 'Clusters'])
+
+    if admLvl == 'State' :
+        map_path = "maps/ind_adm_shp/IND_adm2.shp"
+        name = 'NAME_1'
+    elif admLvl == 'District' :
+        map_path = "maps/ind_adm_shp/IND_adm2.shp"
+        name = 'NAME_2'
+    else :
+        map_path = "maps/ind_adm_shp/IND_adm3.shp"
+        name = 'NAME_3'
+
+    map_gdf = gpd.read_file(map_path)
+    merged = map_gdf.set_index(name).join(df_reduced.set_index(admLvl))
+
+    fig, ax = plt.subplots(1, figsize=(12, 12))
+    ax.axis('off')
+    ax.set_title('Clustering with k-means, averaged on each district',
+                fontdict={'fontsize': '15', 'fontweight' : '3'})
+    fig = merged.plot(column='Clusters', cmap='RdYlGn', linewidth=0.5, ax=ax, edgecolor='0.2',legend=True)
